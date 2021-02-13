b2a721cb0fdb4746b1918115bfa84f4a7d7bbc4f
Juhani Markkula <jmarkkula@gmail.com>
2021-02-13 14:47:24 +0200

    some upgrades

--- src/BroBlametor/ClientApp/src/components/Sevens.js
+++ src/BroBlametor/ClientApp/src/components/Sevens.js
@@ -1,30 +1,33 @@
 import React from 'react';
 import TickTable from './TickTable';
 import { Bar } from 'react-chartjs-2';
-import { DateTime } from "luxon";
-import { getDateRange } from "../helpers/dateHelper";
+import { DateTime } from 'luxon';
+import { getDateRange } from '../helpers/dateHelper';
 
-const grades = ["7a", "7a+", "7b", "7b+", "7c", "7c+"];
+const grades = ['7a', '7a+', '7b', '7b+', '7c', '7c+'];
 const sevenA = 11;
 const eightA = 17;
 
 const getQuartalSevens = (ticks) => {
   const { starts, ends } = getDateRange(ticks);
   const years = getYears(starts.year, ends.year);
-  const quarters = years.flatMap(year => getQuarters(year));
+  const quarters = years.flatMap((year) => getQuarters(year));
 
-  return quarters.map(q => {
-    const sevens = ticks.filter(t => t.timestamp >= q.starts && t.timestamp <= q.ends);
-    const gradeCounts = grades.map(g => ({ grade: g, count: sevens.filter(t => t.grade.name === g).length }));
-    const gradeCountMap = gradeCounts.reduce((x, gc) => { x[gc.grade] = gc.count; return x; }, {})
+  return quarters.map((q) => {
+    const sevens = ticks.filter((t) => t.timestamp >= q.starts && t.timestamp <= q.ends);
+    const gradeCounts = grades.map((g) => ({ grade: g, count: sevens.filter((t) => t.grade.name === g).length }));
+    const gradeCountMap = gradeCounts.reduce((x, gc) => {
+      x[gc.grade] = gc.count;
+      return x;
+    }, {});
 
     return {
       ...q,
       sevens: {
         total: sevens.length,
-        ...gradeCountMap
-      }
-    }
+        ...gradeCountMap,
+      },
+    };
   });
 };
 
@@ -40,7 +43,7 @@ const getQuarters = (year) => {
       year: year,
       quarter: i + 1,
       starts: qStart,
-      ends: qEnd
+      ends: qEnd,
     });
   }
 
@@ -60,36 +63,39 @@ const getYears = (startYear, endYear) => {
 };
 
 export const Sevens = (props) => {
-  const ticks = props.ticks.filter(t => t.grade.id >= sevenA);
+  const ticks = props.ticks.filter((t) => t.grade.id >= sevenA);
 
   if (ticks.length === 0) {
     return <h4>Plz send harder.</h4>;
   }
 
   const quartalSevens = getQuartalSevens(ticks);
-  console.info(quartalSevens);
 
   const options = {
     legend: {
-      display: true
+      display: true,
     },
     scales: {
-      xAxes: [{
-        stacked: true,
-      }],
-      yAxes: [{
-        stacked: true
-      }]
-    }
+      xAxes: [
+        {
+          stacked: true,
+        },
+      ],
+      yAxes: [
+        {
+          stacked: true,
+        },
+      ],
+    },
   };
 
   const data = {
-    labels: quartalSevens.map(s => s.quarter === 1 ? (s.year + " Q" + s.quarter) : (" Q" + s.quarter)),
-    datasets: grades.map(grade => ({
+    labels: quartalSevens.map((s) => (s.quarter === 1 ? s.year + ' Q' + s.quarter : ' Q' + s.quarter)),
+    datasets: grades.map((grade) => ({
       label: grade,
-      backgroundColor: `rgba(255,0,0, ${0.3 + (0.12 * grades.indexOf(grade))})`,
-      data: quartalSevens.map(s => s.sevens[grade])
-    }))
+      backgroundColor: `rgba(255,0,0, ${0.3 + 0.12 * grades.indexOf(grade)})`,
+      data: quartalSevens.map((s) => s.sevens[grade]),
+    })),
   };
 
   return (
@@ -97,7 +103,7 @@ export const Sevens = (props) => {
       <Bar data={data} options={options} />
       <TickTable ticks={ticks} />
     </React.Fragment>
-  )
+  );
 };
 
 export default Sevens;
